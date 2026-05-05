333c535f4cc8e464f065d4d78d4841e5551ae599
ashiqul290 <ashiquldeveloper11@gmail.com>
2026-05-05 19:12:07 +0600

    category navbar

--- src/Common/Header.jsx
+++ src/Common/Header.jsx
@@ -5,6 +5,81 @@ import { IoIosSearch } from "react-icons/io";
 import { CiHeart, CiUser } from "react-icons/ci";
 import union from "../assets/HeaderImg/Union.png";
 export const Header = () => {
+ const categories = [
+  {
+    name: "Women's Fashion",
+    subcategories: [
+      {
+        name: "Saree",
+        items: [
+          "Cotton","Silk","Katan","Muslin","Jamdani",
+          "Georgette","Nokshi Kantha","Linen","Khadi",
+          "Tangail Taat","Monipuri","Dhakai Jamdani"
+        ]
+      },
+      {
+        name: "Clothes",
+        items: [
+          "T-shirt","Shorts","Shirt","Track suit",
+          "Jeans","Georgette","Waistcoat",
+          "Sweater","Coats","Suit","Sweatshirt","Uniform"
+        ]
+      },
+      {
+        name: "Cosmetic",
+        items: [
+          "Perfume","Eye make-up","Skin care","Hair care",
+          "Make-up","Oral Care","Body Care","Sanitary pad",
+          "Shower gels & creams","Hair Removal Products",
+          "Lipstick","Suntan cream"
+        ]
+      },
+      {
+        name: "Sports & Outdoor",
+        items: [
+          "Sweatshirt","T-shirt","Sports Bra","Leggings",
+          "Tracksuit","Running Shoes","Sports Bag",
+          "Sports Equipment","Outdoor Shoes","Snow Boot",
+          "Outdoor Equipment","Sports Accessories"
+        ]
+      },
+      {
+        name: "Personal Care",
+        items: [
+          "Perfume","Sexual Health","After Shave Products","Razor blade"
+        ]
+      },
+      {
+        name: "Shoes",
+        items: [
+          "Heeled shoes","sneakers","Casual Shoes","Babet",
+          "Sandals","Slippers"
+        ]
+      }
+    ]
+  },
+  {
+    name: "Men's Fashion",
+  },
+  {
+    name: "Home & Lifestyle",
+  },
+  {
+    name: "Sports & Outdoor",
+  },
+  {
+    name: "Health & Beauty",
+  },
+  {
+    name: "Arts & Crafts",
+  },
+  {
+    name: "Computer & Electronics",
+  },
+  {
+    name: "Food & Grocery",
+  },
+];
   return (
     <>
       <div className=" border-b border-gray-100">
@@ -37,6 +112,17 @@ export const Header = () => {
                 <img src={union} alt="Cart" /> my Cart
               </button>
             </div>
+          </div>
+          <div className="mb-2.5">
+            <ul className=" flex justify-between ">
+              {
+                categories.map((item)=>(
+
+                  <li className="text-[16px] text-[#333333] relative uppercase font-medium cursor-pointer hover:text-[#0970CD] duration-500 after:content-[''] after:absolute after:left-0 after:bottom-0
+            after:h-0.5 after:w-0 after:bg-[#0970CD] after:duration-500 hover:after:w-full">{item.name}</li>
+                ))
+              }
+            </ul>
           </div>
         </Container>
       </div>
