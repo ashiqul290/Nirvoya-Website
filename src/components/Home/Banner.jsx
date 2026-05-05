dbe10b91e38f11723c1d0b9c4c7b6d5d664bef82
ashiqul290 <ashiquldeveloper11@gmail.com>
2026-05-05 21:56:37 +0600

    swiper add

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -2,26 +2,102 @@ import React from "react";
 import { Container } from "../../Common/Container";
 import Groceres from "../../assets/Banner/placeholder.png";
 import beauty from "../../assets/Banner/placeholder (1).png";
-import sliderimg from '../../assets/Banner/image.png'
+import sliderimg from "../../assets/Banner/image.png";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export const Banner = () => {
-  
   return (
     <>
       <div className=" py-2.5">
         <Container>
           <div className="flex gap-6.75 items-center">
-   
-              <div className="w-231.25 h-140.75 rounded-[10px] relative">
-                <img src={sliderimg} alt="" />
-                <div className=" absolute top-37 left-15">
-                    <h1 className=" text-[#0970CD] font-bold text-[45px] font-montserrat max-w-121.5 ">Explore Men’s Winter Collection</h1>
-                    <p className=" text-[20px] font-poppins font-normal leading-10 text-[#646464] w-104.5 my-6.75">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. </p>
-                    <button className=" px-6.25 py-2.5 text-white bg-[#0198E9] rounded-[5px] font-poppins font-semibold text-[16px]">SHOP NOW</button>
-                </div>
-              </div>
-              
-     
+            {/* LEFT SLIDER */}
+            <div className="w-[65%]">
+              <Swiper
+                modules={[Navigation, Pagination, Autoplay]}
+                spaceBetween={20}
+                slidesPerView={1}
+                //   navigation
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 3000 }}
+                className="w-full h-full"
+              >
+                <SwiperSlide>
+                  <div className="w-231.25 h-140.75 rounded-[10px] relative">
+                    <img src={sliderimg} alt="" />
+                    <div className=" absolute top-37 left-15">
+                      <h1 className=" text-[#0970CD] font-bold text-[45px] font-montserrat max-w-121.5 ">
+                        Explore Men’s Winter Collection
+                      </h1>
+                      <p className=" text-[20px] font-poppins font-normal leading-10 text-[#646464] w-104.5 my-6.75">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor.{" "}
+                      </p>
+                      <button className=" px-6.25 py-2.5 text-white bg-[#0198E9] rounded-[5px] font-poppins font-semibold text-[16px]">
+                        SHOP NOW
+                      </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="w-231.25 h-140.75 rounded-[10px] relative">
+                    <img src={sliderimg} alt="" />
+                    <div className=" absolute top-37 left-15">
+                      <h1 className=" text-[#0970CD] font-bold text-[45px] font-montserrat max-w-121.5 ">
+                        Explore Men’s Winter Collection
+                      </h1>
+                      <p className=" text-[20px] font-poppins font-normal leading-10 text-[#646464] w-104.5 my-6.75">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor.{" "}
+                      </p>
+                      <button className=" px-6.25 py-2.5 text-white bg-[#0198E9] rounded-[5px] font-poppins font-semibold text-[16px]">
+                        SHOP NOW
+                      </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="w-231.25 h-140.75 rounded-[10px] relative">
+                    <img src={sliderimg} alt="" />
+                    <div className=" absolute top-37 left-15">
+                      <h1 className=" text-[#0970CD] font-bold text-[45px] font-montserrat max-w-121.5 ">
+                        Explore Men’s Winter Collection
+                      </h1>
+                      <p className=" text-[20px] font-poppins font-normal leading-10 text-[#646464] w-104.5 my-6.75">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor.{" "}
+                      </p>
+                      <button className=" px-6.25 py-2.5 text-white bg-[#0198E9] rounded-[5px] font-poppins font-semibold text-[16px]">
+                        SHOP NOW
+                      </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="w-231.25 h-140.75 rounded-[10px] relative">
+                    <img src={sliderimg} alt="" />
+                    <div className=" absolute top-37 left-15">
+                      <h1 className=" text-[#0970CD] font-bold text-[45px] font-montserrat max-w-121.5 ">
+                        Explore Men’s Winter Collection
+                      </h1>
+                      <p className=" text-[20px] font-poppins font-normal leading-10 text-[#646464] w-104.5 my-6.75">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor.{" "}
+                      </p>
+                      <button className=" px-6.25 py-2.5 text-white bg-[#0198E9] rounded-[5px] font-poppins font-semibold text-[16px]">
+                        SHOP NOW
+                      </button>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </Swiper>
+            </div>
 
             <div className="">
               <div className="w-md h-66.75  rounded-[10px] relative">
